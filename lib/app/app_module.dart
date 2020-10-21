c4beac71a3707d72d1ed63a34deff63013eb4b65
Bruno Ribeiro <brf147@gmail.com>
2020-10-21 07:45:34 -0400

    Correção de Declarações no AppModule

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,10 +2,9 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:paga_facil/app/app_controller.dart';
 import 'package:paga_facil/app/app_widget.dart';
+import 'package:paga_facil/app/modules/home/home_module.dart';
+import 'package:paga_facil/app/modules/login/login_module.dart';
 import 'package:paga_facil/app/pages/splash_screen.dart';
-
-import 'modules/home/home_widget.dart';
-import 'modules/login/login_widget.dart';
 
 class AppModule extends MainModule {
   @override
@@ -14,8 +13,8 @@ class AppModule extends MainModule {
   @override
   List<ModularRouter> get routers => [
         ModularRouter('/', child: (_, args) => SplashScreen()),
-        ModularRouter('/login', child: (_, args) => LoginPage()),
-        ModularRouter('/home', child: (_, args) => HomePage()),
+        ModularRouter('/login', module: LoginModule()),
+        ModularRouter('/home', module: HomeModule()),
       ];
 
   @override
